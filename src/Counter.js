79f5671c8cf1a6f73dfe7e6ff29e421f8c4d14ca
unknown <qudtls1717@gmail.com>
2020-07-18 03:19:42 +0900

    complete-Mobx

---
+++ src/Counter.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { observer, inject } from "mobx-react";
+
+@inject("store")
+@observer
+class Counter extends Component {
+  render() {
+    const { store } = this.props;
+    return (
+      <div>
+        <h1>{store.number}</h1>
+        <button onClick={store.increase}>+1</button>
+        <button onClick={store.decrease}>-1</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
---
+++ src/stores/Store.js
@@ -0,0 +1,13 @@
+import { observable, action } from "mobx";
+
+export default class Store {
+  @observable number = 0;
+
+  @action increase = () => {
+    this.number++;
+  };
+
+  @action decrease = () => {
+    this.number--;
+  };
+}
